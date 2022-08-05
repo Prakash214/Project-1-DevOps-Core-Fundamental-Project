e8146a7ee57dc3ca1b8f8a91d71f1aadde183514
prakash214 <prakashthapa1998@gmail.com>
2022-08-05 02:09:00 +0000

    finished?

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -13,21 +13,13 @@ pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report=html
 
 
-ssh jenkins@prod-server <<EOF
 
-if [ -d anime-app ]; then
-    cd anime-app && git pull origin main
-else
-    git clone https://github.com/Prakash214/animelist.git anime-app
-    cd anime-app
-fi
+while getopts "c" opt; do
+    case ${opt} in 
+        c) echo -e "python3 create.py" >> deploy-steps;;
+    esac
+done 
 
-sudo apt install python3 python3-pip python3-venv  -y
-
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-python3 create.py
-python3 -m gunicorn -D --bind 0.0.0.0:5000 --workers 4 app:app
-EOF
+echo "python3 -m gunicorn -D --bind 0.0.0.0:5000 --workers 4 app:app" >> deploy-steps;;
+ssh jenkins@prod-server < deploy-steps
 
