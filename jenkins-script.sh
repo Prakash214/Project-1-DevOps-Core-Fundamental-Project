61635b0c12758b7ffcb53f5cf9adc5059f9c03f0
prakash214 <prakashthapa1998@gmail.com>
2022-08-05 01:30:35 +0000

    update deplo

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -12,5 +12,21 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report=html
 
+
+ssh jenkins@prod-server <<EOF
+
+IF [ -d anime-app ]; then
+ cd animelist && git pull origin main
+else
+    git clone https://github.com/Prakash214/animelist.git anime-app
+    cd anime-app
+fi
+sudo apt install python3 python3-pip python3-venv  -y
+
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 python3 create.py
-python3 app.py
+python3 -m gunicorn -D --bind 0.0.0.0:5000 --workers 4 app:app
+EOF
+
